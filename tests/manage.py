390225ff45e1f792ea0e731a2d7af55c1b1af07c
Matthew Galvis <mg@tsl.io>
2020-10-13 15:58:31 -0400

    Neo4J-update-t-4.1

--- tests/manage.py
+++ tests/manage.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+
+import django
+
+django.setup()
+
 import sys
+
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
